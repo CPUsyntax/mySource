4d8990c2eff461d2415a25fefba0fe7768f0b622
CPUsyntax <cpusyntax@gmail.com>
2020-05-25 10:56:01 -0400

    modified check_ip/sh

--- scripts/check_ip.sh
+++ scripts/check_ip.sh
@@ -4,5 +4,6 @@ testy="$(cat /home/cpu/myip)"
 if [ $newip != $testy ]
 then
         echo ${newip} > /home/cpu/myip
-        sendemail -q -f cpusyntax@gmail.com -t cpusyntax@gmail.com -u "Subject: ${newip}" -m "WEB SERVER IP CHANGE" -s smtp.gmail.com:587 -o tls=yes -xu cpusyntax@gmail.com -xp tyJesus68
+        sendemail -q -f cpusyntax@gmail.com -t cpusyntax@gmail.com -u "Subject: ${newip}" -m "WEB SERVER IP CHANGE" \
+	-s smtp.gmail.com:587 -o tls=yes -xu EMAIL@gmail.com -xp PASSWORD
 fi
